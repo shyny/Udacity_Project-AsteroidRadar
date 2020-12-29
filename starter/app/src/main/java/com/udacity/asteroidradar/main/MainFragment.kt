3279996059abaf853a8c41ce90c6839925f62b19
shyny <shyaka@fastwebnet.it>
2020-12-29 19:16:44 +0100

    Details Implementation

--- starter/app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
@@ -3,7 +3,9 @@ package com.udacity.asteroidradar.main
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.udacity.asteroidradar.R
 import com.udacity.asteroidradar.databinding.FragmentMainBinding
 
@@ -22,6 +24,13 @@ class MainFragment : Fragment() {
 
         binding.asteroidRecycler.adapter = AsteroidListAdapter(AsteroidListAdapter.AsteroidClickListener{
             viewModel.displayAsteroidDetails(it)
+        })
+
+        viewModel.navigateToSelectedAsteroid.observe(viewLifecycleOwner, {
+            if(it != null){
+                this.findNavController().navigate(MainFragmentDirections.actionShowDetail(it))
+                viewModel.displayAsteroidDetailsComplete()
+            }
         })
 
         setHasOptionsMenu(true)
